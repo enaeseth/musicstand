e541f7dd5a46e1655f290391fccead216667c8e7
Eric Naeseth <eric@naeseth.com>
2009-10-10 17:37:31 +0000

    Fixing the note->frequency stuff.

--- notes/notes.py
+++ notes/notes.py
@@ -5,26 +5,51 @@ Find the frequencies of chromatic scale notes.
 """
 
 import re
+import math
+
+def note_to_semitone(octave, note, accidental):
+    notes = {
+        'A': 0,
+        'B': 2,
+        'C': -9,
+        'D': -7,
+        'E': -5,
+        'F': -4,
+        'G': -2
+    }
+    
+    semitone = (octave - 4) * 12 + notes[note]
+    print "(%d - 4 * 12 + %d) = %d" % (octave, notes[note], semitone)
+    if accidental in ('b', '♭', 'flat'):
+        semitone -= 1
+    elif accidental in ('#', '♯', 'sharp'):
+        semitone += 1
+    return semitone
 
 _reference_freq = 440.0 # use A440 as the reference note
 _scale_constant = 2 ** (1.0/12)
-def note_to_freq(octave, note, accidental):
-    notes = dict((n, 1 - i)
-        for i, n in enumerate(('B', 'A', 'G', 'F', 'E', 'D', 'C')))
-    
-    semitone = (octave - 4) * 12 + notes[note] * 2
-    #if accidental in ('b', '♭'):
-    if accidental == 'flat':
-        if note == 'F':
-            raise ValueError('F-flat does not exist')
-        semitone -= 1
-    #elif accidental in ('#', '♯'):
-    elif accidental == 'sharp':
-        if note == 'E':
-            raise ValueError('E-sharp does not exist')
-        semitone += 1
-    
+def semitone_to_freq(semitone):
     return _reference_freq * (_scale_constant ** semitone)
+
+def note_to_freq(octave, note, accidental):
+    return semitone_to_freq(note_to_semitone(octave, note, accidental))
+
+def freq_to_semitone(frequency):
+    def round(value):
+        def pair(transformer):
+            transformed = transformer(value)
+            return (abs(value - transformed), int(transformed))
+        return min((pair(math.floor), pair(math.ceil)))
+    
+    raw = math.log(frequency / _reference_freq, 2) * 12
+    semitone, error = round(raw)
+    
+    return semitone, error
+
+def semitone_to_note(semitone):
+    return semitone
+    # return divmod(semitone + 10, 12)
+    
 
 _note_pattern = re.compile(r'^([A-G])([b♭#♯-♮])?(\d+)$')
 def parse_note(note_description):
