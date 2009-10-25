d2e8e88d988758e1062a9f73059c24216cd46feb
Ben Farley <benjamintfarley@gmail.com>
2009-10-25 21:58:45 +0000

    Parser.awesomeness += 1

--- lilypondParser.py
+++ lilypondParser.py
@@ -5,12 +5,13 @@
 # By Emily and Ben, 10/3/09
 
 import sys
+import math
 from string import punctuation
 
 class Note():
 	def __init__(self, octavo, meas, dur, num):
 		self.pitch = 0
-		self.number = num
+		self.beatnumber = num
 		self.measure = meas
 		self.duration = dur
 		self.accidental = False
@@ -24,42 +25,50 @@ class Note():
 			self.accidental = 'flat'
 		for char in noteData:
 			if char.isdigit():
-				self.duration = char
+				self.duration = float(char)
 			elif char == '\'':
 				self.octave += 1
 			elif char == ',':
 				self.octave -= 1
 				
-	def printNote(self):
-		noteArray = [self.measure, self.number, self.duration, [self.pitch, self.octave, self.accidental]]
-		print noteArray
+	def printNote(self,bigNoteArray):
+		bigNoteArray.append([self.measure, self.beatnumber, self.duration, [self.octave, self.pitch, self.accidental]])
 		
 def parseFile(filename):
 	notes = []
+	bigNoteArray = []
 	file = open(filename, 'r')
 	for line in file:
 		list = line.split()
 		if list:
-			if list[0].isalpha() or list[0] == "<":
+			if list[0][0].isalpha() or list[0] == "<":
 				for i in list:
 					if i[0].isalpha():
 						notes.append(i)
+						
 	file.close()
 	octavo = 4
-	measure = 0
-	duration = 4
+	measure = 1.00
+	duration = 4.0
 	number = 1
 	for note in notes:
-		thisNote = Note(octavo, measure, duration, number)
+		measure = str(measure)
+		parts = measure.split('.')
+		beatnumber = "."+parts[1]
+		measure = float(measure)
+		measureNum = parts[0]
+		thisNote = Note(octavo, float(measureNum), float(duration), float(beatnumber))
 		thisNote.parseNote(note)
-		thisNote.printNote()
+		thisNote.printNote(bigNoteArray)
 		number += 1
 		octavo = thisNote.octave
 		measure = measure + (1/float(thisNote.duration))
 		duration = thisNote.duration
+		
+	return bigNoteArray
 	
 	
 if __name__ == '__main__':
 	filename = sys.argv[1]
-	parseFile(filename)
+	notes = parseFile(filename)
 	
