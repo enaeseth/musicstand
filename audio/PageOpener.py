491d3d2a2384032267827520329e27a95e2c9030
Emily Kawaler <e.kawaler@gmail.com>
2009-10-27 20:50:51 +0000

    Put in the new page opener.

---
+++ audio/PageOpener.py
@@ -0,0 +1,8 @@
+#This thing opens files. It has one function, for whatever reason.
+#If it eats its vegetables, someday it will grow up and be able to close files too.
+
+import os
+
+def openPage(self, measNum):
+		pdfname = "test"+str(measNum)+".pdf"
+		os.system("open "+filename)
