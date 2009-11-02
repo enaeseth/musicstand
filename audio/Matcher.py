d8eb778f5549c7ebbdc7f42e076972f34f9c3fe7
David Tillery <tillerda@carleton.edu>
2009-11-02 21:55:52 +0000

    Cleaning up the matcher.

--- audio/Matcher.py
+++ audio/Matcher.py
@@ -1,113 +1,95 @@
 from __future__ import with_statement
+
+import sys
 import re
-import lilypondParser
-import PageOpener
-import mutex as mu
+import notes
+from lilypondParser import parseFile as parse_lilypond
+from pages import open_page
+from collections import deque
+from threading import Condition
 
-class Matcher():
-
-	def __init__(self, filename):
-		self.incomingNotes = [] #The queue!
-		self.lockQueue = mu.mutex()
-		self.lilypond_tuples = lilypondParser.masterMethod(filename)
-		self.current_location = 0
-		self.num_misses = 0
-
-	'''def get_lp_tuples(self, filename):
-		"""
-		Reads the text file with the given filename and returns an array of 
-		the tuples in that file. Emily, feel free to ignore this and use your 
-		array instead.
-		"""
-		with open(filename, 'rt') as f:
-			lilypond_tuples = []
-			for line in f:
-				parts = [part.strip() for part in line.split(',')]
-				
-				note = parts[3:]
-				note[0] = int(note[0])
-				if len(note) == 2:
-					note.append(None) # no accidental
-				
-				timing = map(int, parts[:3])
-				timing.append([tuple(note)])
-				lilypond_tuples.append(tuple(timing))
-			return lilypond_tuples'''
-			
-	
-	def add(self, freq):
-		#print 'I AM IN ADD'
-		note = [(4, "D", "flat")]
-		self.lockQueue.lock(self.incomingNotes.append,note)
-		while self.lockQueue.test():
-			self.lockQueue.unlock()
-	
-	def match(self, new_note):
-		"""
-		Takes the parsed Lilypond array, the current location in the array (as an
-		integer), the new note as a tuple inside a list like [(octave,note,accidental)],
-		and the current number of misses in a row. 
-		
-		Returns an updated location and updated number of misses. (If the number of 
-		misses isn't currently 0 and the new note looks correct, the number of misses
-		goes down by 1.)
-		"""
-		if new_note == self.lilypond_tuples[self.current_location][3]:
-			self.num_misses = max(self.num_misses - 1, 0)
-		elif new_note == self.lilypond_tuples[self.current_location+1][3]:
-			self.current_location += 1
-			self.num_misses = max(self.num_misses - 1, 0)
-		else:
-			self.num_misses += 1
-	
-	
-	
-	def run(self):
-		# This is my makeshift queue.
-		'''q = []
-		q.append([(4, "D", "flat")])
-		q.append([(4, "E", "flat")])
-		q.append([(4, "F", None)])
-		q.append([(4, "D", None)])        # wrong note
-		q.append([(4, "D", "flat")])      # wrong note
-		q.append([(4, "D", "flat")])      # wrong note
-		q.append([(4, "G", "flat")])
-		q.append([(4, "G", "flat")])
-		q.append(False)                    # I'm assuming that when the audio runs out,
-										  # David and Eric will pass me a False.
-										  #incorrect assumption, but we can try-- David'''
-										  
-		try:
-			while True:
-				done = False
-				new_note = False
-				while not done:
-					if self.lockQueue.testandset():
-						if len(self.incomingNotes) != 0:
-							new_note = self.incomingNotes.pop(0)
-							self.lockQueue.unlock()
-							done = True
-				if new_note == False:
-					break
-				else:
-					print "New note:", new_note
-					self.match(new_note)
-					print "Current location in the array:", self.current_location 
-					print "Current measure:", self.lilypond_tuples[self.current_location][0]
-					print '------------------------'
-					'''
-					print "Current number of misses:", self.num_misses
-					if self.num_misses > 2:
-						print "THIS IS VERY BAD. INSERT NATHAN'S CODE HERE"
-					print
-					'''
-					#PageOpener.openPage(int(self.lilypond_tuples[self.current_location][0]))
-		except KeyboardInterrupt:
-			pass
-	
-	
+class Matcher(object):
+    def __init__(self, filename, debug=False):
+        self.incoming_notes = deque()
+        self.note_available = Condition()
+        self.lilypond_tuples = parse_lilypond(filename)
+        self.current_location = None
+        self.miss_count = None
+        self.debug_enabled = debug
+        self.running = False
+    
+    def add(self, freq):
+        #print 'I AM IN ADD'
+        note = [(4, "D", "flat")] # notes.freq_to_note(freq) # fo' realz.
+        
+        if self.running:
+            self.incoming_notes.append(note)
+            with self.note_available:
+                self.note_available.notify()
+    
+    def match(self, new_note):
+        """
+        Takes the parsed Lilypond array, the current location in the array (as an
+        integer), the new note as a tuple inside a list like [(octave,note,accidental)],
+        and the current number of misses in a row. 
+        
+        Returns an updated location and updated number of misses. (If the number of 
+        misses isn't currently 0 and the new note looks correct, the number of misses
+        goes down by 1.)
+        """
+        
+        if new_note == self.lilypond_tuples[self.current_location][3]:
+            self.miss_count = max(self.miss_count - 1, 0)
+        elif new_note == self.lilypond_tuples[self.current_location+1][3]:
+            self.current_location += 1
+            self.miss_count = max(self.miss_count - 1, 0)
+        else:
+            self.miss_count += 1
+    
+    def debug(self, message, *args):
+        if self.debug_enabled:
+            print >>sys.stderr, message % args
+    
+    def run(self):
+        self.running = True
+        self.incoming_notes.clear()
+        self.current_location = 0
+        self.miss_count = 0
+        
+        def get_next_note():
+            try:
+                return self.incoming_notes.popleft()
+            except IndexError:
+                # no notes are available; wait until we're told that one is ready
+                with self.note_available:
+                    self.note_available.wait()
+                
+                return get_next_note() if self.running else None
+        
+        while self.running:
+            new_note = get_next_note()
+            self.debug('New note: %s', new_note)
+            if new_note is None:
+                break
+            
+            self.match(new_note)
+            self.debug('Current location in array: %d', self.current_location)
+            self.debug('Current measure: %d', self.lilypond_tuples[self.current_location][0])
+            self.debug('-' * 20)
+            
+            # if self.miss_count > 2:
+            #     # nathan.do_something_useful()
+            
+            # open_page(self.lilypond_tuples[self.current_location][0])
+    
+    def shutdown(self):
+        if not self.running:
+            raise RuntimeError('what exactly am I supposed to shut down?!')
+        
+        self.running = False
+        with self.note_available:
+            self.note_available.notify() # wake up our thread if it's waiting for this
 
 if __name__ == '__main__':
-	newMatch = Matcher("test.ly")
-	newMatch.run()
-	
+    matcher = Matcher("test.ly")
+    matcher.run()
