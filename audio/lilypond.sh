3ece7cc19092025975bd1bc2d7ec37874cf1d375
Ben Farley <benjamintfarley@gmail.com>
2009-11-04 21:02:00 +0000

    Fixing a bit of an oops: adding lilypond.sh.

---
+++ audio/lilypond.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+clear
+
+filename=$(basename "${1##*/}" .ly)
+directory=$PWD"/"
+#echo "${directory}"$filename".pdf"
+lilypondPDFoutput="${directory}"$filename".pdf"
+#lilypondMIDIoutput="${directory}"/$filename".midi"
+
+#echo $1
+#echo $filename
+#echo $directory
+#echo $lilypondPDFoutput
+#echo "done"
+
+#key="1"
+
+#do
+~/Desktop/LilyPond.app/Contents/Resources/bin/lilypond -o "${directory}/${filename}" $1
+#wait
+#open "${lilypondPDFoutput}"
+#open "${lilypondMIDIoutput}"
+#echo "****************************************************************"
+#echo "Continue to work. When finished, save your document and choose :"
+#echo "****************************************************************"
+#echo "Refresh: 1"
+#echo "Quit: 2"
+#read key
+#done
+#if [ $key = "2" ]
+#then exit
+exit
+#fi
+
+
+# /Applications/LilyPond.app/Contents/Resources/bin/lilypond -o "${directory}" "${fullpath}"
