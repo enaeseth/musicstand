deb0d891e79db47aaa8a5512d06a39f1cf653785
Eric Naeseth <eric@naeseth.com>
2009-12-30 17:30:15 +0000

    Uh, I didn't really mean to leave those commented out…

--- mstand/musicstand.py
+++ mstand/musicstand.py
@@ -8,11 +8,11 @@ An intelligent music stand.
 
 from __future__ import with_statement
 
-# from mstand.monitor import Monitor
-# from mstand.analyzer import Analyzer
-# from mstand.lilypondParser import parse_file as parse_lilypond_file
-# from mstand.pages import open_page
-# from mstand.match.matcher import Matcher
+from mstand.monitor import Monitor
+from mstand.analyzer import Analyzer
+from mstand.lilypondParser import parse_file as parse_lilypond_file
+from mstand.pages import open_page
+from mstand.match.matcher import Matcher
 from mstand.match.algorithm import Algorithm
 
 import re
