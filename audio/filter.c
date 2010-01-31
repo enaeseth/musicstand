b476c17c4b9de248396da58326131d4a47828988
Eric Naeseth <eric@naeseth.com>
2010-01-31 04:20:20 +0000

    Move the FilterType_SUBCLASS macro into filter.h.

--- audio/filter.c
+++ audio/filter.c
@@ -326,47 +326,4 @@ PyTypeObject FilterType = {
     0,                         /* tp_new */
 };
 
-#define FilterType_SUBCLASS(name, methods, doc) \
-     PyTypeObject name ## Type = { \
-         PyObject_HEAD_INIT(NULL) \
-         0,                         /*ob_size*/ \
-         "audio." #name,            /*tp_name*/ \
-         sizeof(name ## Object),    /*tp_basicsize*/ \
-         0,                         /*tp_itemsize*/ \
-         (destructor)name ## _dealloc, /*tp_dealloc*/ \
-         0,                         /*tp_print*/ \
-         0,                         /*tp_getattr*/ \
-         0,                         /*tp_setattr*/ \
-         0,                         /*tp_compare*/ \
-         (reprfunc)name ## _repr,   /*tp_repr*/ \
-         0,                         /*tp_as_number*/ \
-         0,                         /*tp_as_sequence*/ \
-         0,                         /*tp_as_mapping*/ \
-         0,                         /*tp_hash */ \
-         0,                         /*tp_call*/ \
-         0,                         /*tp_str*/ \
-         0,                         /*tp_getattro*/ \
-         0,                         /*tp_setattro*/ \
-         0,                         /*tp_as_buffer*/ \
-         Py_TPFLAGS_DEFAULT,        /*tp_flags*/ \
-         doc,                       /* tp_doc */ \
-         0,                         /* tp_traverse */ \
-         0,                         /* tp_clear */ \
-         0,                         /* tp_richcompare */ \
-         0,                         /* tp_weaklistoffset */ \
-         0,                         /* tp_iter */ \
-         0,                         /* tp_iternext */ \
-         0,                         /* tp_methods */ \
-         0,                         /* tp_members */ \
-         0,                         /* tp_getset */ \
-         &FilterType,               /* tp_base */ \
-         0,                         /* tp_dict */ \
-         0,                         /* tp_descr_get */ \
-         0,                         /* tp_descr_set */ \
-         0,                         /* tp_dictoffset */ \
-         (initproc)name ## _init,   /* tp_init */ \
-         0,                         /* tp_alloc */ \
-         name ## _new,             /* tp_new */ \
-     }
-
 FilterType_SUBCLASS(PythonFilter, 0, "A filter implemented in Python.");
