527bcd3bb599eca3f28348ba314204a4e7dc1a04
Eric Naeseth <eric@naeseth.com>
2010-02-04 05:07:25 +0000

    ...turns out the interpreter did not function at all. Well, uh, now it does.

--- mstand/interpreter.py
+++ mstand/interpreter.py
@@ -7,39 +7,42 @@
             Find all the shit that it could be, by looking at the overtones and shit. Return that shit. Be like, "HEY! THIS THING HAS SEVERAL HARMONICS!"
 '''
 
-from notes import note_to_freq
+from notes import *
 
-class Interpreter():
+class Interpreter(object):
     def __init__(self):
-        self.heard_freqs = []
+        # heard_notes stores the notes heard in frequency decomposition
+        # we store notes instead of frequencies to work around floating-point
+        # issues
+        self.heard_notes = set()
 
-    def overtones(self, cur_freq):
-        '''This actually checks for the notes in the list of heard frequencies.'''
-        chord = True
-        for note in cur_freq:
-            cur_over = note
-            while cur_over <= note*6:
-                if cur_over not in heard:
-                    chord = False
-                else:
-                    cur_over = cur_over + note
-        if not chord:
-            return freq_to_note(heard[0])
-        else:
-            return True
+    def overtones(self, frequencies):
+        """
+        For all the given frequencies, check if either that frequency or one
+        of its first few harmonics has been heard.
+        """
+        
+        def has_overtone(freq):
+            """
+            Checks if a single frequency has really been "heard".
+            """
+            
+            # check if the note was heard, or any of its first five harmonics
+            # were heard
+            return any(freq_to_note(freq * i) in self.heard_notes
+                for i in xrange(1, 7))
+        
+        # check that has_overtone returns true for all the frequencies
+        return all(has_overtone(freq) for freq in frequencies)
         
     def heard(self, heard_frequencies):
         '''The FFT thing should call this.'''
-        heard = []
-        for pair in self.heard_freqs:
-            heard.append(pair[0])
-        self.heard_freqs = heard
+        self.heard_notes = set(freq_to_note(freq)
+            for freq, intensity in heard_frequencies)
         
-    def lookslike(self, current_notes):
+    def looks_like(self, current_notes):
         '''The matcher should call this. It will return True to the matcher
         if any harmonic of the note has been heard, and False otherwise.
         Currently, chords need to be 100% correct to get a True.'''
-        cur_freq = []
-        for note in current_notes:
-            cur_freq.append(note_to_freq(note[0], note[1], note[2]))
-        return overtones(cur_freq)
+        
+        return self.overtones(note_to_freq(*note) for note in current_notes)
