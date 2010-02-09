69ef41000bf9995647b7fdc36e975ebf8e0db4f3
Eric Naeseth <eric@naeseth.com>
2010-02-09 20:04:40 +0000

    Upgrade the matcher tester to programmatically verify match results.

--- matcher_tester.py
+++ matcher_tester.py
@@ -2,10 +2,13 @@
 # encoding: utf-8
 
 from __future__ import with_statement
+from mstand.interpreter import Interpreter
 from mstand.notes import *
 from mstand.newParser import parse_file
-from mstand.match.matcher import Matcher
+from mstand.match.matcher import Matcher, Interval
 from mstand.match.algorithm import Algorithm
+from mstand.terminal import color
+from Queue import Queue
 from time import sleep
 import re
 
@@ -17,15 +20,30 @@ def read_note_file(filename):
         return [note_to_freq(*parse_note(part)) for part in parts]
     
     with open(filename, 'rt') as f:
+        expected_position = -1
         for line in f:
             line = line.strip()
-            if line.lower().startswith('pause'):
-                match = re.search(r'(\d+(?:\.\d+))$', line)
-                if match is None:
-                    raise ValueError(line)
-                instructions.append(('pause', float(match.group(1))))
+            
+            if line.endswith('+'):
+                expected_position += 1
+            elif line.endswith('.'):
+                # stay put
+                pass
             else:
-                instructions.append(('play', parse_line_notes(line)))
+                parts = re.split(r'\s*([-=]>|➔|➞|➝)\s*', line)
+                if len(parts) > 1:
+                    expected_position = int(parts[2])
+                    line = parts[0]
+                else:
+                    # default to increasing the position
+                    expected_position += 1
+            
+            if line.lower().startswith('pause'):
+                command = ('pause', None, expected_position)
+            else:
+                command = ('play', parse_line_notes(line), expected_position)
+            
+            instructions.append(command)
     
     return instructions
 
@@ -76,6 +94,85 @@ def parse_algorithm_options(raw_list):
     
     return options
 
+class Tester(object):
+    SHUTDOWN_SENTINEL = object()
+    
+    def __init__(self, notes, instructions, algorithm, debug=False):
+        self.matcher = Matcher(notes, algorithm, None, Interpreter(),
+            self.position_changed, progress_listener=self.matched,
+            debug=options.debug)
+        self.instructions = instructions
+        
+        self._position_queue = Queue()
+    
+    def position_changed(self, matcher):
+        pass
+    
+    def matched(self, notes, position):
+        sleep(0.25)
+        self._position_queue.put(position)
+    
+    def test(self):
+        self.matcher.start()
+        
+        try:
+            self._run()
+        finally:
+            self.matcher.shutdown()
+            while self.matcher.running:
+                sleep(0.2)
+    
+    def _run(self):
+        last_expected_pos = -1
+        for action, arg, expected_pos in self.instructions:
+            if action == 'pause':
+                print '--> Playing silence.'
+                self.matcher.add([])
+            elif action == 'play':
+                print '--> Playing %s.' % \
+                    ', '.join(unparse_note(*freq_to_note(f)) for f in arg)
+                self.matcher.add(arg)
+            else:
+                raise ValueError(action)
+            
+            new_pos = self._position_queue.get()
+            if new_pos == last_expected_pos:
+                message = 'Stayed at'
+            else:
+                message = 'Moved to'
+            
+            if new_pos != expected_pos:
+                text_color = 'red'
+                stop = True
+                expectation_string = '; expected %d' % expected_pos
+            else:
+                text_color = 'green'
+                stop = False
+                expectation_string = ''
+            
+            if new_pos >= 0:
+                interval = self.matcher.intervals[new_pos]
+            else:
+                interval = Interval(-1.0, 0.0, [])
+            
+            if new_pos == last_expected_pos:
+                message = 'Stayed at'
+            else:
+                message = 'Moved to'
+            
+            note_string = ('' if not interval.notes else
+                '; ' +
+                    ', '.join(unparse_note(*note) for note in interval.notes))
+            print color(text_color, '<-- %s %d (%.3f:%.3f%s)%s.',
+                message, new_pos, interval.start, interval.end, note_string,
+                expectation_string)
+            
+            if stop:
+                break
+            
+            last_expected_pos = expected_pos
+            sleep(0.5)
+
 if __name__ == '__main__':
     from optparse import OptionParser
     
@@ -90,11 +187,14 @@ if __name__ == '__main__':
     
     options, args = parser.parse_args()
     
-    instructions = read_note_file(args[0])
+    try:
+        instructions = read_note_file(args[0])
     
-    if not args[1].endswith('.ly'):
-        parser.error('looks like you did that backwards')
-    notes = parse_file(args[1])
+        if not args[1].endswith('.ly'):
+            parser.error('looks like you did that backwards')
+        notes = parse_file(args[1])
+    except IndexError:
+        parser.error('missing an argument')
     
     # Load the matching algorithm implementation
     try:
@@ -104,30 +204,5 @@ if __name__ == '__main__':
     except ValueError, e:
         parser.error(e[0])
     
-    def position_changed(matcher):
-        interval = matcher.current_interval
-        if interval is None:
-            print '<-- Matching started.'
-        else:
-            print '<-- Moved to %.3f (%s).' % (interval.start,
-            ', '.join(unparse_note(*n) for n in interval.notes))
-    
-    matcher = Matcher(notes, algorithm, position_changed, options.debug)
-    matcher.start()
-    try:
-        for action, arg in instructions:
-            if action == 'pause':
-                print '--> Playing silence for %.2f seconds.' % arg
-                matcher.add([])
-                sleep(arg)
-            elif action == 'play':
-                print '--> Played', \
-                    ', '.join(unparse_note(*freq_to_note(f)) for f in arg)
-                matcher.add(arg)
-                sleep(0.5)
-            else:
-                raise ValueError(action)
-    finally:
-        matcher.shutdown()
-        while matcher.running:
-            sleep(0.2)
+    tester = Tester(notes, instructions, algorithm, options.debug)
+    tester.test()
