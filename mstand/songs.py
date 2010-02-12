fb1d88a57b10db24afcde812b175c3a126533fc4
Eric Naeseth <eric@naeseth.com>
2010-02-12 18:58:15 +0000

    Tabs be gone! GONE!

--- mstand/songs.py
+++ mstand/songs.py
@@ -46,42 +46,42 @@ def add_song(title):
         print >>f, title
 
 def create_lilypond_files(file_path, song_name):
-	'''Given a path to a lilypond file and the desired name of the song, creates
-	a directory for that song in our Songs folder, copies the lilypond file to
-	it, and runs the lilypond script to create .ps, .pdf, and .midi files.'''
-	
-	cache_dir = get_song_path(song_name)
-	file_name = os.path.basename(file_path)
-	
-	if os.path.exists(cache_dir):
-		# Song has already been previously added
-		return
-	else:
-		# Song is new - make a directory for it and copy the .ly file to it
-		os.makedirs(cache_dir)
-		
-		subprocess.check_call(['cp', file_path, cache_dir])
+    '''Given a path to a lilypond file and the desired name of the song, creates
+    a directory for that song in our Songs folder, copies the lilypond file to
+    it, and runs the lilypond script to create .ps, .pdf, and .midi files.'''
+    
+    cache_dir = get_song_path(song_name)
+    file_name = os.path.basename(file_path)
+    
+    if os.path.exists(cache_dir):
+        # Song has already been previously added
+        return
+    else:
+        # Song is new - make a directory for it and copy the .ly file to it
+        os.makedirs(cache_dir)
+        
+        subprocess.check_call(['cp', file_path, cache_dir])
 
-	lilypond_path = os.path.join(os.path.dirname(__file__), 'lilypond.sh')
-	new_path = os.path.join(cache_dir, file_name)
-	
-	# OPEN THE LILYPOND FILE AND LOOK THROUGH IT TO MAKE SURE IT HAS 
-	# \midi AND \layout. IF IT DOES, FINE. IF IT DOESN'T, ADD THEM
-	# AND OVERWRITE THE FILE
-	
-	# Run lilypond script
-	subprocess.check_call([lilypond_path, new_path])
-	
-	# Convert pdf to pictures
-	file_base = os.path.splitext(file_name)[0]
-	pdf_name = os.path.join(cache_dir, file_base + ".pdf")
-	png_name = os.path.join(cache_dir, file_base + ".png")
-		
-	# Convert PDF to pictures
-	print "Converting PDF to images...",
-	os.system("convert -density 400 " + pdf_name + " -resize 25% " + png_name )
-	print "done."
+    lilypond_path = os.path.join(os.path.dirname(__file__), 'lilypond.sh')
+    new_path = os.path.join(cache_dir, file_name)
+    
+    # OPEN THE LILYPOND FILE AND LOOK THROUGH IT TO MAKE SURE IT HAS 
+    # \midi AND \layout. IF IT DOES, FINE. IF IT DOESN'T, ADD THEM
+    # AND OVERWRITE THE FILE
+    
+    # Run lilypond script
+    subprocess.check_call([lilypond_path, new_path])
+    
+    # Convert pdf to pictures
+    file_base = os.path.splitext(file_name)[0]
+    pdf_name = os.path.join(cache_dir, file_base + ".pdf")
+    png_name = os.path.join(cache_dir, file_base + ".png")
+        
+    # Convert PDF to pictures
+    print "Converting PDF to images...",
+    os.system("convert -density 400 " + pdf_name + " -resize 25% " + png_name )
+    print "done."
 
 if __name__ == '__main__':
-	create_lilypond_files("~/Desktop/march.ly", "ImperialMarch")
-	
+    create_lilypond_files("~/Desktop/march.ly", "ImperialMarch")
+    
