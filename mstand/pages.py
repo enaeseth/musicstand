04e7e0584fe334360d3898fe1e6d7423d2b576fd
Eric Naeseth <eric@naeseth.com>
2010-02-20 22:41:37 +0000

    Deleting the defunct pages module.

--- mstand/pages.py
+++
@@ -1,9 +0,0 @@
-#This thing opens files. It has one function, for whatever reason.
-#If it eats its vegetables, someday it will grow up and be able to close files too.
-
-import os
-
-def open_page(filename, measure_number, cache_dir):
-    base, ext = os.path.splitext(os.path.basename(filename))
-    path = os.path.join(cache_dir, '%s.%d.pdf' % (base, measure_number))
-    os.system('open "%s"' % path)
