bdb206d1ae12546ef4e4cbb72845bf44285772ff
Eric Naeseth <enaeseth@gmail.com>
2010-03-16 22:00:42 -0500

    Made the lilypond.sh script work from any location.

--- mstand/lilypond.sh
+++ mstand/lilypond.sh
@@ -17,8 +17,24 @@ lilypondPDFoutput="${directory}"$filename".pdf"
 
 #do
 echo "${directory}/${filename}"
+
 my_home=`dirname "$0"`
-"$my_home/LilyPond.app/Contents/Resources/bin/lilypond" --ps --pdf -o "${directory}/${filename}" "$1"
+
+if [ -d "$my_home/LilyPond.app" ]; then
+  lilypond_home="$my_home/LilyPond.app"
+elif [ -d "/Applications/LilyPond.app" ]; then
+  lilypond_home="/Applications/LilyPond.app"
+else
+  echo "error: cannot find LilyPond directory; is it installed?" >&2
+  exit 1
+fi
+
+if [ ! -x "$lilypond_home/Contents/Resources/bin/lilypond" ]; then
+  echo "error: cannot find LilyPond executable; is it installed?" >&2
+  exit 1
+fi
+
+"$lilypond_home/Contents/Resources/bin/lilypond" --ps --pdf -o "${directory}/${filename}" "$1"
 #wait
 #open "${lilypondPDFoutput}"
 #open "${lilypondMIDIoutput}"
